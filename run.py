54c16ab1b19cf07c2932156802a91ee25bd45c7b
Issam-A <issamally0@gmail.com>
2023-06-24 22:24:07 +0100

    Add get_user_input function for guess row and guess column

--- run.py
+++ run.py
@@ -11,7 +11,8 @@ class Seaship_Battle:
         self.board_size = board_size
         self.num_ships = num_ships
         self.username = username
-      
+        self.hits = []
+        self.misses = []
     
 
     def print_board(self):
@@ -20,7 +21,18 @@ class Seaship_Battle:
         """
         print('**********')
         print(f"{self.username} : {turns}\n ")
-        print(" 0 1 2 3 4 5")
+        print('**********')
+        for i in range(self.board_size):
+            row = ' '
+            for j in range(self.board_size):
+                coor = (i, j)
+                symb = ' . '
+                if coor in self.hits:
+                    symb = 'X'
+                elif coor in self.misses:'3 '
+                symb = '-'
+                row = row + symb
+            print(x, row)
 
 
 
@@ -40,3 +52,28 @@ class Seaship_Battle:
         return ships
     
     
+    def get_user_input(self):
+        """
+        
+        """
+        try:
+            guess_row = input("Enter a row between 0-6: \n")
+            while guess_row not in ['0', '1', '2', '3', '4', '5', '6']:
+                print("Input invalid. Please enter a valid row.")
+                guess_row = input("Enter a row between 0-6: ")
+
+            guess_col = input("Enter a column between 0-6: \n")
+            while guess_col not in ['0', '1', '2', '3', '4', '5', '6']:
+                print("Input invalid. Please enter a valid column.")
+                guess_col = input("Enter a column between 0-6: \n")
+
+            return int(guess_row), int(guess_col) 
+
+
+
+
+
+
+
+game = Seaship_Battle(board_size=7, num_ships=3, username="Player1")
+Ships = game.create_ships()
